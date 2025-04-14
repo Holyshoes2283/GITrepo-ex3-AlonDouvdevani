0cdefeb1f0ff48946095b18da0ae303b57929fd8
Holyshoes2283 <168278983+Holyshoes2283@users.noreply.github.com>
2025-04-14 23:20:41 +0300

    Update Alon.java

--- Alon.java
+++ Alon.java
@@ -1,7 +1,7 @@
 public class Alon {
     public static void main(String[] args) {
         for(int i = 1; i <= 10; i++) {
-            System.out.println("Alon");
+            System.out.println("Alon change");
         }
     }
 }
